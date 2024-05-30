bce0aa36e79388d4cbde68da6f2a03b1f9adb8f0
jeon Seungwon <wjstmddnjs5@daum.net>
2024-05-30 15:16:11 +0900

    싫어요 버튼 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,29 @@ import { useState } from "react";
 
 function App() {
   const [like, setLike] = useState(false);
+  const [dislike, setDislike] = useState(false);
 
   const onClick = () => setLike((prev) => !prev);
-  
+
   const 좋아요또는취소버튼 = like ? (
-  <button onClick={onClick}>좋아요 취소</button>
+    <button onClick={onClick}>좋아요 취소</button>
   ) : (
-  <button onClick={onClick}>좋아요</button>
-  )
+    <button onClick={onClick}>좋아요</button>
+  );
+
+  
+  const 싫어요또는취소버튼 = like ? (
+    <button onClick={onClick}>싫어요 취소</button>
+  ) : (
+    <button onClick={onClick}>싫어요</button>
+  );
+
   return (
     <div>
-      <h1>좋아요/싫어요 버튼 구현</h1>
-      {좋아요또는취소버튼}
+      
+        <h1>좋아요/싫어요 버튼 구현</h1>
+        {좋아요또는취소버튼} {싫어요또는취소버튼}
+      
     </div>
   );
 }
